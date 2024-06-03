a34eb022b8367bef76a94a82766cfd5487d606dc
stormymayday <stormymayday7@gmail.com>
2024-06-03 15:43:00 -0700

    JSON Middleware

--- server.js
+++ server.js
@@ -2,8 +2,16 @@ import express from "express";
 
 const app = express();
 
+// JSON Middleware
+app.use(express.json());
+
 app.get("/", (req, res) => {
     res.send("Hello World");
+});
+
+app.post("/", (req, res) => {
+    console.log(req);
+    res.json({ message: "data received", data: req.body });
 });
 
 app.listen(5100, () => {
