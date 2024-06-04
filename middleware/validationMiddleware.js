d39527c260544b25fc75697aee1f98893b3873cf
stormymayday <stormymayday7@gmail.com>
2024-06-03 22:20:49 -0700

    Validation Middleware

---
+++ middleware/validationMiddleware.js
@@ -0,0 +1,32 @@
+import { body, validationResult } from "express-validator";
+import { BadRequestError } from "../errors/customErrors.js";
+
+const withValidationErrors = (validateValues) => {
+    return [
+        validateValues,
+
+        (req, res, next) => {
+            const errors = validationResult(req);
+
+            if (!errors.isEmpty()) {
+                const errorMessages = errors.array().map((error) => {
+                    return error.msg;
+                });
+
+                throw new BadRequestError(errorMessages);
+            }
+
+            next();
+        },
+    ];
+};
+
+// For every controller:
+export const validateTest = withValidationErrors([
+    body("name")
+        .notEmpty()
+        .withMessage("name is required")
+        .isLength({ min: 3, max: 50 })
+        .withMessage("name must be between 3 and 50 characters")
+        .trim(),
+]);
