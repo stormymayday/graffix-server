1cc6a45be0dc12e9456f2af66bc9c7a5dbab2eec
stormymayday <stormymayday7@gmail.com>
2024-06-03 23:41:02 -0700

    User Model

---
+++ models/UserModel.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema(
+    {
+        username: String,
+        email: String,
+        password: String,
+        name: String,
+        bio: String,
+        profilePictureUrl: String,
+        role: {
+            type: String,
+            enum: ["artlover", "artist", "admin"],
+            default: "artlover",
+        },
+        // followers: [ObjectId] (references to Users)
+        // following: [ObjectId] (references to Users)
+    },
+    { timestamps: true }
+);
+
+export default mongoose.model("User", UserSchema);
