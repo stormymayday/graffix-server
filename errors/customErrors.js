be7fbe567ad7e120518e9b7a4912408c8d118817
stormymayday <stormymayday7@gmail.com>
2024-06-03 21:50:58 -0700

    More Custom Errors

--- errors/customErrors.js
+++ errors/customErrors.js
@@ -1,9 +1,37 @@
 import { StatusCodes } from "http-status-codes";
 
+// 404
 export class NotFoundError extends Error {
     constructor(message) {
         super(message);
         this.name = "NotFoundError";
         this.statusCode = StatusCodes.NOT_FOUND;
+    }
+}
+
+// 400
+export class BadRequestError extends Error {
+    constructor(message) {
+        super(message);
+        this.name = "BadRequestError";
+        this.statusCode = StatusCodes.BAD_REQUEST;
+    }
+}
+
+// 401
+export class UnauthenticatedError extends Error {
+    constructor(message) {
+        super(message);
+        this.name = "UnauthenticatedError";
+        this.statusCode = StatusCodes.UNAUTHORIZED;
+    }
+}
+
+// 403
+export class UnauthorizedError extends Error {
+    constructor(message) {
+        super(message);
+        this.name = "UnauthorizedError";
+        this.statusCode = StatusCodes.FORBIDDEN;
     }
 }
