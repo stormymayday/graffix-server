79148cf80159f3791b04ff56d11704231f5bb25c
stormymayday <stormymayday7@gmail.com>
2024-06-12 23:57:05 -0700

    using ES modules

--- middleware/multerMiddleware.js
+++ middleware/multerMiddleware.js
@@ -18,7 +18,13 @@
 import multer from "multer";
 import fs from "fs";
 import path from "path";
+import { fileURLToPath } from "url";
 
+// Convert import.meta.url to a file path
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Ensure the upload directory exists
 const uploadDir = path.join(__dirname, "..", "public/uploads");
 if (!fs.existsSync(uploadDir)) {
     fs.mkdirSync(uploadDir, { recursive: true });
