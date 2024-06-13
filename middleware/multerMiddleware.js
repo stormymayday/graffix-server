d277992659e8b816a6d14c1e6204e809036e609d
stormymayday <stormymayday7@gmail.com>
2024-06-12 23:52:50 -0700

    multerMiddleware

--- middleware/multerMiddleware.js
+++ middleware/multerMiddleware.js
@@ -1,9 +1,32 @@
+// import multer from "multer";
+
+// const storage = multer.diskStorage({
+//     destination: (req, file, cb) => {
+//         // callback(null, "public/uploads");
+//         cb(null, "public/uploads/");
+//     },
+//     filename: (req, file, cb) => {
+//         const fileName = file.originalname;
+//         cb(null, fileName);
+//     },
+// });
+
+// const fileUpload = multer({ storage });
+
+// export default fileUpload;
+
 import multer from "multer";
+import fs from "fs";
+import path from "path";
+
+const uploadDir = path.join(__dirname, "..", "public/uploads");
+if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir, { recursive: true });
+}
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        // callback(null, "public/uploads");
-        cb(null, "public/uploads/");
+        cb(null, uploadDir);
     },
     filename: (req, file, cb) => {
         const fileName = file.originalname;
