8073f48fc24096854d213879dda9563844f1c64c
stormymayday <stormymayday7@gmail.com>
2024-06-12 21:33:21 -0700

    Testing public folder path

--- middleware/multerMiddleware.js
+++ middleware/multerMiddleware.js
@@ -2,7 +2,8 @@ import multer from "multer";
 
 const storage = multer.diskStorage({
     destination: (req, file, callback) => {
-        callback(null, "public/uploads");
+        // callback(null, "public/uploads");
+        callback(null, "public/uploads/");
     },
     filename: (req, file, callback) => {
         const fileName = file.originalname;
